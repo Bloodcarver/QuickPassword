f22fd99334015c715e80d303bd876981a9ae20cb
Bloodcarver <hg@fritsla.com>
2018-11-03 12:29:10 +0100

    Application  now generates a bunch of passwords and if you click them they populate your clipboard and the form closes, click the form and it reloads.

--- QuickPassword/Form1.cs
+++ QuickPassword/Form1.cs
@@ -16,5 +16,94 @@ namespace QuickPassword
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Reload();
+        }
+
+        private void Reload()
+        {
+            label1.Text = GetPassword(6);
+            label2.Text = GetPassword(8);
+            label3.Text = GetPassword(10);
+            label4.Text = GetPassword(12);
+            label5.Text = GetPassword(14);
+            label6.Text = RandomPassword.Generate(8, 8);
+            label7.Text = RandomPassword.Generate(10, 10);
+            label8.Text = RandomPassword.Generate(12, 12);
+            label9.Text = RandomPassword.Generate(14, 14);
+        }
+        private string GetPassword(int iLength)
+        {
+            string sPassword = "";
+            var ppg = new PwdGen.PronounceablePasswordGenerator();
+            var passwords = ppg.Generate(PasswordCount: 1, PasswordLength: iLength);
+            foreach (var password in passwords)
+            {
+                sPassword= password.ToString();
+            }
+            ppg = null;
+            return sPassword;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label1.Text);
+            this.Close();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Reload();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label2.Text);
+            this.Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label3.Text);
+            this.Close();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label4.Text);
+            this.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label5.Text);
+            this.Close();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label6.Text);
+            this.Close();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label7.Text);
+            this.Close();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label8.Text);
+            this.Close();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label9.Text);
+            this.Close();
+        }
     }
 }
