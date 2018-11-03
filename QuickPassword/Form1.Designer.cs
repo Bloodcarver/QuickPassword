58ed7d7c0398d1227f5b01156524352712190e7c
Bloodcarver <hg@fritsla.com>
2018-11-03 20:17:36 +0100

    Smaller form

--- QuickPassword/Form1.Designer.cs
+++ QuickPassword/Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.label1.AutoSize = true;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label1.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(39, 41);
+            this.label1.Location = new System.Drawing.Point(39, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(152, 43);
             this.label1.TabIndex = 0;
@@ -58,7 +58,7 @@
             this.label2.AutoSize = true;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(39, 82);
+            this.label2.Location = new System.Drawing.Point(39, 57);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(152, 43);
             this.label2.TabIndex = 1;
@@ -70,7 +70,7 @@
             this.label3.AutoSize = true;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label3.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(39, 123);
+            this.label3.Location = new System.Drawing.Point(39, 98);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(152, 43);
             this.label3.TabIndex = 2;
@@ -82,7 +82,7 @@
             this.label4.AutoSize = true;
             this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label4.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(39, 164);
+            this.label4.Location = new System.Drawing.Point(39, 139);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(152, 43);
             this.label4.TabIndex = 3;
@@ -94,7 +94,7 @@
             this.label5.AutoSize = true;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(39, 205);
+            this.label5.Location = new System.Drawing.Point(39, 180);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(152, 43);
             this.label5.TabIndex = 4;
@@ -106,7 +106,7 @@
             this.label6.AutoSize = true;
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label6.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(39, 246);
+            this.label6.Location = new System.Drawing.Point(39, 221);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(152, 43);
             this.label6.TabIndex = 5;
@@ -118,7 +118,7 @@
             this.label7.AutoSize = true;
             this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label7.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(39, 287);
+            this.label7.Location = new System.Drawing.Point(39, 262);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(152, 43);
             this.label7.TabIndex = 6;
@@ -130,7 +130,7 @@
             this.label8.AutoSize = true;
             this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label8.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(39, 328);
+            this.label8.Location = new System.Drawing.Point(39, 303);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(152, 43);
             this.label8.TabIndex = 7;
@@ -142,7 +142,8 @@
             this.label9.AutoSize = true;
             this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label9.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(39, 369);
+            this.label9.Location = new System.Drawing.Point(39, 344);
+            this.label9.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(152, 43);
             this.label9.TabIndex = 8;
@@ -154,7 +155,8 @@
             this.label10.AutoSize = true;
             this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label10.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(753, 5);
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(549, 9);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(41, 41);
             this.label10.TabIndex = 9;
@@ -166,7 +168,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 400);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
